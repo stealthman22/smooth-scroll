c33b5821a9d50f0a47099da3c4dac97bff2bfe0b
stealthman22 <tdotonline@gmail.com>
2020-01-28 00:12:00 +0000

    Built with traversy's approach, jQuery did not work some error with the global

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-//  Option 2 jQuery
-$('.nav-links').on('click', function (e) {
-    console.log(this.hash)
+/* //  Option 2 jQuery
+$('.navbar a').on('click', function (e) {
+    // if hash value is not empty, prevent default of the event
     if (this.hash !== '') {
         e.preventDefault();
 
         const hash = this.hash;
 
-        $('html, body').animate({
+        $('html body').animate({
+            //  animate when you scroll down from the top
             scrollTop: $(hash).offset().top
         }, 800);
+        console.log(hash)
     }
-})
+
+}) */
+
+
+// Option 3
+//  Smooth scroll
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    speed: 1000
+});
+
+console.log(scroll)
